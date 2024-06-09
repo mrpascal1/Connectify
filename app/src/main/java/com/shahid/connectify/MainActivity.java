6b40fd52537593864f1b4abd6480a944b0e4abfb
Z3ntaki <dbzkronos@gmail.com>
2024-06-09 19:04:28 +0530

    commit number 420 added a new comment

--- app/src/main/java/com/shahid/connectify/MainActivity.java
+++ app/src/main/java/com/shahid/connectify/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//newer line comment
 //afifa
         // New line comment
 
