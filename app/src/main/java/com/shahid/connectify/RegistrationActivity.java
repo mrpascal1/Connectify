d74f97e8f89af8ffab043b7c5984a44d327ab9b4
Mohammed Afshaan <mohammed.afshaan06@gmail.com>
2024-06-22 08:10:44 +0530

    Changes

--- app/src/main/java/com/shahid/connectify/RegistrationActivity.java
+++ app/src/main/java/com/shahid/connectify/RegistrationActivity.java
@@ -3,13 +3,21 @@ package com.shahid.connectify;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.shahid.connectify.databinding.ActivityRegisterationPageBinding;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,13 +34,27 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText usernameEditText;
     private EditText reenterPasswordEditText;
     private TextView signInShift;
+    FirebaseAuth mAuth;
 
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            /*
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+            */
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityRegisterationPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mAuth = FirebaseAuth.getInstance();
         // Initialize views using View Binding
         registerButton = binding.registerButton;
         emailEditText = binding.email;
@@ -205,5 +227,31 @@ public class RegistrationActivity extends AppCompatActivity {
                 .addOnFailureListener(e -> {
                     Toast.makeText(RegistrationActivity.this, "Registration failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
                 });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email = String.valueOf(emailEditText.getText());
+                password = String.valueOf(passwordEditText.getText());
+
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegistrationActivity.this, "Account Created.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
     }
+
 }
--- app/src/main/java/com/shahid/connectify/SignInActivity.java
+++ app/src/main/java/com/shahid/connectify/SignInActivity.java
@@ -2,25 +2,95 @@ package com.shahid.connectify;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private EditText emailEditTxt;
+    private EditText passwordEditTxt;
+    private TextView LoginButton;
     private TextView NewUserTextView;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            /*
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+            */
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page);
-
+        emailEditTxt = findViewById(R.id.username);
+        passwordEditTxt = findViewById(R.id.password);
         NewUserTextView = findViewById(R.id.sign_in_note);
+        LoginButton = findViewById(R.id.login_button);
 
         NewUserTextView.setOnClickListener(view -> {
             Intent intent = new Intent(SignInActivity.this, RegistrationActivity.class);
             startActivity(intent);
             finish();
+        });
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email = String.valueOf(emailEditTxt.getText());
+                password = String.valueOf(passwordEditTxt.getText());
+
+                if(TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignInActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(SignInActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignInActivity.this, "Sign In Successful", Toast.LENGTH_SHORT).show();
+                                    /*
+                                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                    */
+                                } else {
+
+                                    Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
         });
     }
 }
